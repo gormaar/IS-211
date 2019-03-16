e71f8212d08a46084b1d27ecfca5db1bb36dacb0
gorm <35703118+gormaar@users.noreply.github.com>
2019-03-16 12:16:54 +0100

    Update Main.java

--- Regatta/Main.java
+++ Regatta/Main.java
@@ -2,9 +2,9 @@ package Regatta;
 
 public class Main {
 
-   /* public static void main(String[] args)  {
+    public static void main(String[] args)  {
 
         Regatta regatta = new Regatta();
         regatta.simulator();
-    }*/
+    }
 }
