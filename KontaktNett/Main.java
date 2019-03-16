9ce9fccd725760f66af41ca51f6fd2d9e2c7543e
gorm <35703118+gormaar@users.noreply.github.com>
2019-03-16 12:18:24 +0100

    Add files via upload

---
+++ KontaktNett/Main.java
@@ -0,0 +1,13 @@
+package KontaktNett;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        KontaktNett kontaktnett = new KontaktNett();
+
+        //kontaktnett.registrerSamtale("Espen", "Prinsessa");
+        //kontaktnett.finnDirekteKontakter("Narren", 5);
+        kontaktnett.finnAlleKontakter("Narren", 19);
+
+    }
+}
