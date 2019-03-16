65c5712a63a68c302df8fe08f4a22f616eb3b5be
gorm <35703118+gormaar@users.noreply.github.com>
2019-03-16 12:22:40 +0100

    Add files via upload

---
+++ EventSimulator/Supermarket/CustomerEnter.java
@@ -0,0 +1,19 @@
+package Supermarket;
+
+import EventSimulator.*;
+
+public class CustomerEnter extends Event {
+
+
+    public CustomerEnter(Customer customer, int time) {
+
+        super(customer.getEnter(), customer);
+
+    }
+
+    public Event happen()   {
+        int time = super.getTime();
+        System.out.println("Customer " + customer.getCustNr() + " just entered the store --- " + "time: " + time);
+        return new CustomerLookForCheckout(customer, time);
+    }
+}
